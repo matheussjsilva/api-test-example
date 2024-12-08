d9f3e23c46526db8e7699f67311dd56d7e5630f1
Matheus Silva <matheus.silva.contato26@gmail.com>
2024-12-08 17:16:45 -0300

    add .env

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import express from "express";
 import { StatusCodes } from "http-status-codes";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
 const PORT = process.env.PORT || 3000;
+console.log(process.env.PORT);
 
 app.listen(PORT, () => {
   console.log(`Rodando em http://localhost:${PORT}`);
