35f240df2d70737a4b8cc5ce6d7434cc68cfdaf0
Matheus Silva <matheus.silva.contato26@gmail.com>
2024-12-08 16:37:55 -0300

    first commit, teste api

---
+++ index.js
@@ -0,0 +1,74 @@
+import express from "express";
+import { StatusCodes } from "http-status-codes";
+
+const app = express();
+app.use(express.json());
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Rodando em http://localhost:${PORT}`);
+});
+
+let users = [
+  {
+    id: 1,
+    name: "Rafael",
+    age: 41,
+  },
+  {
+    id: 2,
+    name: "Jose",
+    age: 56,
+  },
+  {
+    id: 3,
+    name: "Carlos Alberto",
+    age: 23,
+  },
+];
+app.get("/", (request, response) => {
+  response.send(`
+    <h1>Trabalhando com express</h1>
+    `);
+});
+
+app.get("/users", (request, response) => {
+  response.send(users);
+});
+
+app.get("/users/:userId", (request, response) => {
+  const userId = request.params.userId;
+  const user = users.find((user) => {
+    return user.id === Number(userId);
+  });
+  return response.send(user);
+});
+
+app.post("/users", (request, response) => {
+  const newUser = request.body;
+  users.push(newUser);
+  return response.status(StatusCodes.CREATED).send(newUser);
+});
+
+app.put("/users/:userId", (request, response) => {
+  const userId = request.params.userId;
+
+  const updatedUser = request.body;
+
+  users = users.map((user) => {
+    if (Number(userId) === user.id) {
+      return updatedUser;
+    }
+    return user;
+  });
+  response.status(200).send(updatedUser);
+});
+
+app.delete("/users/:userId", (request, response) => {
+  const userId = request.params.userId;
+  users = users.filter((user) => {
+    return user.id !== Number(userId);
+  });
+
+  return response.status(StatusCodes.NO_CONTENT).send();
+});
